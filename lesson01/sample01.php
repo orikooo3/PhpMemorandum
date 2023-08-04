ebd76afde13ba26c330db3034b06766ed5549b36
orikopg <ori.kou.1025@gmail.com>
2023-08-04 09:36:12 +0900

    エスケープシーケンスと初めての画面表示

---
+++ lesson01/sample01.php
@@ -0,0 +1,17 @@
+<!DOCTYPE html>
+<html lang="ja">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>sample01</title>
+</head>
+
+<body>
+    <!--エスケープシーケンス-->
+    <?php
+    echo "I'm \"studying\"";
+    ?>
+</body>
+
+</html>
