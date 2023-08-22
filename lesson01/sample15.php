366ae74836e9b71426695930d55879875a8e9977
oriko <ori.kou.1025@gmail.com>
2023-08-23 08:03:54 +0900

    update sample15

--- lesson01/sample15.php
+++ lesson01/sample15.php
@@ -6,3 +6,13 @@ if ($success !== false) {
 } else {
     echo 'ファイルの読み込みに失敗しました';
 }
+?>
+
+<?php
+$success = file_put_contents('data/test.txt', 'これはテストです');
+
+if ($success !== false) :
+    echo 'ファイルの読み込みが完了しました';
+else :
+    echo 'ファイルの読み込みに失敗しました';
+endif; ?>
